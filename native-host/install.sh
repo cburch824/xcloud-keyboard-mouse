ce8632436c2fe815fd07f9bb65b383d1c597bdbe
chrisburch <cburch@appliedsystems.com>
2022-07-03 15:02:08 -0500

    Update for new host file, add test action endpoint

---
+++ native-host/install.sh
@@ -0,0 +1,2 @@
+# Installs the native messaging host. This allows the extension to establish a connection and start the host.
+cp ./../manifest/com.xcloud.xcloud_keyboard_mouse.json ~/.config/google-chrome/NativeMessagingHosts/
