2072e60961d67aee8534f7e36d2067c1b961fdff
chrisburch <cburch@appliedsystems.com>
2022-07-04 12:14:41 -0500

    Log out xcloud listener actions in extension log

---
+++ xcloud-listener/install.sh
@@ -0,0 +1,2 @@
+# Installs the native messaging host. This allows the extension to establish a connection and start the host.
+cp config/com.cburch.xcloud_listener.json ~/.config/google-chrome/NativeMessagingHosts/
